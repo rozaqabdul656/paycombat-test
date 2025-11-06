2461d082aef8a5ec8346c7cec744594b9720b45c
donnypaycombat <donny@paycombat.com>
2025-11-06 15:16:45 +0700

    skip lint on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isVercel = process.env.VERCEL === "1";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  ignoreDuringBuilds: isVercel,
 };
 
 export default nextConfig;
